a03af5d5c509e058a5e45d1ad3085a4adec8a9dd
Melih Firat <melih.firat@ogr.sakarya.edu.tr>
2025-06-11 07:17:18 +0300

    first commit

---
+++ _next/static/4OFai5mt_UhkJ-NRI-iVl/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fapps\u002F[id]","\u002Fapps\u002F[id]\u002Fprivacy-policy","\u002Fapps\u002F[id]\u002Fterms-of-use"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
