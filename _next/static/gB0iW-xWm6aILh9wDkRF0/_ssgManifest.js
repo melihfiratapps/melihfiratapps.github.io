45f827a79ffa99ae06c8df465f2b5af224a81614
Melih Firat <melih.firat@ogr.sakarya.edu.tr>
2026-08-05 15:36:32 +0200

    new era of website

--- _next/static/gB0iW-xWm6aILh9wDkRF0/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fapps\u002F[id]","\u002Fapps\u002F[id]\u002Fprivacy-policy","\u002Fapps\u002F[id]\u002Fterms-of-use"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
